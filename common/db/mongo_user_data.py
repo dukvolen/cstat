52bbd0c641f8c93cc96f39146775c58761ea9568
Andrew Simine <asimin@yandex.ru>
2015-05-18 00:00:02 +0300

    save chages

--- common/db/mongo_user_data.py
+++ common/db/mongo_user_data.py
@@ -4,7 +4,9 @@
 #  @copyright   Copyrighted by Andrew Simine
 #
 #------------------------------------------------
-import pymongo
+from pymongo import MongoClient
+from pymongo.operations import IndexModel
+from pymongo import ASCENDING, DESCENDING
 
 ## Data Access Class
 # @class DataAccess
@@ -13,8 +15,27 @@ class DataAccess(object):
     ## Constructor of the class
     # @def __init__
     # @param username - access DB user
-    # @param passwd - access DB password
-    def __init__(self, user, passwd):
-        self.user = user
-        self.passwd = passwd
-        self.db = pymongo.MongoClient()
+    # @param passwd   - access DB password
+    #------------------------------------------------
+    def __init__(self, userid='cstat', dbhost='127.0.0.1', dbport=27017,
+                 dbuser='statuser', dbpasswd='12345'):
+        self.client = MongoClient(dbhost, dbport)
+        self.db = self.client.get_database(userid) # userid - userid defines name of database
+        self.db.authenticate(dbuser, dbpasswd)
+
+    #------------------------------------------------
+    ## Check category collection
+    # @def prepare_category_collection(self, name)
+    # @param category - name of category
+    def prepare_category_collection(self, category):
+        if(category in self.db.collection_names()):
+            print('Category {0} is already created'.format(category))
+            return self.db.get_collection(category)
+
+        print('Creating category {0}...'.format(category))
+        category = self.db.create_collection(category)
+        category.create_index('date')
+        return category
+
+    #------------------------------------------------
+
--- common/db/tests/mongodb_tests.py
+++ common/db/tests/mongodb_tests.py
@@ -1,1 +1,29 @@
-__author__ = 'asimine'
+#------------------------------------------------
+# @brief        Tests for mongodb class
+# @author       Andrew Simine
+# @copyright    Copyrighted by Andrew Simine
+#
+#------------------------------------------------
+from common.db import mongo_user_data
+import unittest
+
+#------------------------------------------------
+class TestMongoDBDataAccess(unittest.TestCase):
+
+    def setUp(self):
+        self.dataobj = mongo_user_data.DataAccess()
+
+    def testPrepareCategoryCollection(self):
+        retval = self.dataobj.prepare_category_collection('TestCategory')
+        return self.assertIsNotNone(retval, 'Bad test category collection.')
+
+#------------------------------------------------
+def suite():
+    testsuite = unittest.TestSuite()
+    testsuite.addTest(unittest.makeSuite(TestMongoDBDataAccess))
+
+    return testsuite
+
+#------------------------------------------------
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite)
