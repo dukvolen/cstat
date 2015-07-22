8ef0db2ea49642cfe0df222ad93768a21df35455
Andrew Simine <asimin@yandex.ru>
2015-07-22 09:08:39 +0300

    save mongodb functons

--- common/db/mongo_user_data.py
+++ common/db/mongo_user_data.py
@@ -4,9 +4,12 @@
 #  @copyright   Copyrighted by Andrew Simine
 #
 #------------------------------------------------
+import datetime
+
 from pymongo import MongoClient
-from pymongo.operations import IndexModel
-from pymongo import ASCENDING, DESCENDING
+#from pymongo.operations import IndexModel
+import pymongo
+from pymongo.errors import BulkWriteError
 
 ## Data Access Class
 # @class DataAccess
@@ -24,29 +27,110 @@ class DataAccess(object):
         self.db.authenticate(dbuser, dbpasswd)
 
     #------------------------------------------------
+    def __prepare_category_name(self, category_name):
+        return category_name.replace(' ', '_')
+
+    #------------------------------------------------
     ## Check category collection
     # @def prepare_category_collection(self, name)
     # @param category - name of category
     def prepare_category_collection(self, category):
-        if(category in self.db.collection_names()):
-            print('Category {0} is already created'.format(category))
-            return self.db.get_collection(category)
+        category_name = self.__prepare_category_name(category)
+        if(category_name in self.db.collection_names()):
+            print('Category {0} is already created'.format(category_name))
+            return self.db.get_collection(category_name)
 
-        print('Creating category {0}...'.format(category))
-        category = self.db.create_collection(category)
-        category.create_index('date')
-        return category
+        print('Creating category {0}...'.format(category_name))
+        category_item = self.db.create_collection(category_name)
+        category_item.create_index('date')
+
+        return category_item
 
     #------------------------------------------------
-    def save_user_data(self, date):
+    ## Save any data sent by user in database by category
+    # @def save_user_data(self, category, data)
+    # @param data - user's data in JSON format
+    # @param category_name - category of data for save
+    def save_user_data_by_category(self, category_name, data):
+        print('Saving user data for category {} ...'.format(category_name))
         retval = True
+
+        category_collection = self.prepare_category_collection(category_name)
+        bulk_obj = category_collection.initialize_ordered_bulk_op()
+        for param in data:
+            bulk_obj.insert(param)
+
+        try:
+            bulk_obj.execute()
+        except BulkWriteError as bwe:
+            print(bwe.details)
+
         return retval
 
     #------------------------------------------------
-    def get_user_data(self, to_date=None, limit=None):
+    ## Save any data sent by user in database
+    # @def save_user_data(self, data)
+    # @param data - user's data in JSON format
+    def save_user_data(self, data, ignore_errors=True):
+        print('Saving user data...')
         retval = True
-        query = {}
-#        self.db.find()
+
+        try:
+            received_date = data.get('datetime')
+        except Exception as e:
+            print('Error:\n {}'.format(e.args))
+            return False
+
+        # group data by category
+        params_by_cetegory = {}
+        for param in data.get('params'):
+            category_name = param['category']
+            if(category_name is  None):
+                category_name = 'Unknown'
+            else:
+                category_name = self.__prepare_category_name(category_name)
+
+            if(category_name not in params_by_cetegory):
+                params_by_cetegory[category_name] = []
+
+            param['datetime'] = received_date
+            params_by_cetegory[category_name].append(param)
+
+        # save params to database
+        for category_name in params_by_cetegory:
+            try:
+                self.save_user_data_by_category(category_name, params_by_cetegory.get(category_name))
+            except Exception as e:
+                print('Error: \n{}'.format(e.args))
+                if(not ignore_errors):
+                    retval = False
+                    break
+
+        return retval
+
+    #------------------------------------------------
+    ## Get data sent by user from database
+    # @def get_user_data(self, to_date=None, limit=None)
+    # @param to_date - date when user sent reports
+    # @param limit - number of first reports received from user to specified date
+    def get_user_data(self, category, to_date=None, records=None):
+        retval = []
+
+        if(to_date is None):
+            to_date = datetime.datetime.now()
+        to_date2 = to_date + datetime.timedelta(days=1)
+
+        date1 = datetime.datetime(to_date.year, to_date.month, to_date.day, 0, 0)
+        date2 = datetime.datetime(to_date2.year, to_date2.month, to_date2.day, 0, 0)
+
+        category_collection = self.prepare_category_collection(category)
+        query = {'datetime':{'$gte':date1, '$lt':date2}}
+
+        if(records is not None):
+            retval = category_collection.find(query).sort([('datetime', pymongo.ASCENDING)]).limit(records)
+        else:
+            retval = category_collection.find(query).sort([('datetime', pymongo.ASCENDING)])
+
         return retval
 
     #------------------------------------------------
--- common/db/tests/mongodb_tests.py
+++ common/db/tests/mongodb_tests.py
@@ -21,6 +21,8 @@ class TestMongoDBDataAccess(unittest.TestCase):
 
     #--------------------------------------------
     def testSaveData(self):
+        return True
+
         test_data = {'datetime':datetime.datetime(2015, 1, 1, 10, 0),
                      'params': [ {'category':'TestCategory',
                                   'tags':['tag1', 'tag2', 'tag3'],
@@ -46,7 +48,14 @@ class TestMongoDBDataAccess(unittest.TestCase):
                                   'tags':['tag1', 'tag2', 'tag3'],
                                   'param':{'name': 'param5',
                                            'type': 'numeric_array',
-                                           'value': [12, 6, 9]}}]
+                                           'value': [12, 6, 9]}},
+
+                                 {'category':'TestCategory',
+                                  'tags':['tag2', 'tag4', 'tag5'],
+                                  'param':{'name': 'param6',
+                                           'value': {'some_param':'value1',
+                                                     'some_param_2':12,
+                                                     'some_param_3':{'a':1, 'b':'2', 'c':[1, 2, 3, 4]}}}}]
                      }
         retval = self.dataobj.save_user_data(test_data)
         return self.assertTrue(retval, 'Tested data is saved successfully.')
@@ -54,10 +63,19 @@ class TestMongoDBDataAccess(unittest.TestCase):
     #--------------------------------------------
     def testGetData(self):
         test_date = datetime.datetime(2015, 1, 1, 12, 0)
-        retval1 = self.dataobj.get_user_data(to_date=test_date, limit=2)
-        retval2 = self.dataobj.get_user_data(limit=2)
-        retval3 = self.dataobj.get_user_data(to_date=test_date)
-        retval4 = self.dataobj.get_user_data()
+        print('Test 1: category={} date={} records={}'.format('TestCategory', test_date, 2))
+        retval1 = self.dataobj.get_user_data(category = 'TestCategory', to_date=test_date, records=2)
+        print('Test 1: status={}'.format(retval1.count()>=0))
+        print('Test 2: category={} records={}'.format('TestCategory', 2))
+        retval2 = self.dataobj.get_user_data(category = 'TestCategory', records=2)
+        print('Test 2: status={}'.format(retval2.count()>=0))
+        print('Test 3: category={} date={}'.format('TestCategory', test_date))
+        retval3 = self.dataobj.get_user_data(category = 'TestCategory', to_date=test_date)
+        print('Test 3: status={}'.format(retval3.count()>=0))
+        print('Test 4: category={}'.format('TestCategory'))
+        retval4 = self.dataobj.get_user_data(category = 'TestCategory')
+        print('Test 4: status={}'.format(retval4.count()>=0))
+
         return self.assertTrue((retval1 and retval2 and retval3 and retval4), 'Test getting is successfully.')
 
 #------------------------------------------------
